f18efbf3a7cf5cffac6109a2de9ed09317145b9e
Trofimov Maxim <dantezzstudio@gmail.com>
2022-03-23 17:34:53 +0700

    Add clients list

--- handlers/connect.js
+++ handlers/connect.js
@@ -3,11 +3,13 @@ import onClose from "./close"
 
 let num = 0;
 const clients = [];
+
 const onConnect = (client) => {
     num++;
     client._num = num;
 
     console.log("New connection #"+num);
+    clients.push(client);
 
     client.on("message", (message)=>onMessage(client,message));
     client.on("close", ()=>onClose(client))
