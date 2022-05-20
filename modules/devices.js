cf976432dfd66c062497f97f288b535b2497aaaa
Trofimov Maxim <dantezzstudio@gmail.com>
2022-05-20 18:18:59 +0700

    fix: change default map_type for new users

--- modules/devices.js
+++ modules/devices.js
@@ -1,28 +1,28 @@
 import md5 from "md5";
 
-const genMac = () => "XX:XX:XX:XX:XX:XX".replace(/X/g, function() {
+const genMac = () => "XX:XX:XX:XX:XX:XX".replace(/X/g, function () {
     return "0123456789ABCDEF".charAt(Math.floor(Math.random() * 16))
 });
 
 const genHash = () => md5(Math.random().toString(36).substring(7))
 
 const genStartInf = () => {
-    const res = {devices:{},cables:{},connections:{}};
+    const res = { devices: {}, cables: {}, connections: {} };
     const pc = {
-        type:"pc",
-        name:"Компьютер",
-        power:false,
-        position: {x:270,y:699},
-        boot:{
-            priority:[]
+        type: "pc",
+        name: "Компьютер",
+        power: false,
+        position: { x: 270, y: 699 },
+        boot: {
+            priority: []
         },
-        interfaces:{}
+        interfaces: {}
     };
     const socket = {
-        type:"network_socket",
-        name:"Интернет розетка",
-        position:{x:140,y:755},
-        interfaces:{}
+        type: "network_socket",
+        name: "Интернет розетка",
+        position: { x: 140, y: 755 },
+        interfaces: {}
     };
 
     const pcHash = genHash(); // Компьютер
@@ -33,35 +33,35 @@ const genStartInf = () => {
     const cableHash = genHash(); // Интернет-кабель
 
     socket.interfaces = {
-        [socketEthHash]:{
-            type:"ethernet",
-            mac:genMac()
+        [socketEthHash]: {
+            type: "ethernet",
+            mac: genMac()
         },
     };
 
     pc.interfaces = {
-        [ethHash]:{
-            type:"ethernet",
-            mac:genMac()
+        [ethHash]: {
+            type: "ethernet",
+            mac: genMac()
         },
-        [genHash()]:{
-            type:"storage",
-            size:2048,
-            _template:"hios",
-            boot_file:"kernel/init"
+        [genHash()]: {
+            type: "storage",
+            size: 2048,
+            boot_file: "kernel/init",
+            map_type: "hios"
         },
-        [genHash()]:{
-            type:"display",
-            width:800,
-            height:600
+        [genHash()]: {
+            type: "display",
+            width: 800,
+            height: 600
         },
-        [genHash()]:{
-            type:"input"
+        [genHash()]: {
+            type: "input"
         }
     };
 
-    res.cables[cableHash] = {type:"twistedpair"};
-    res.connections[cableHash] = [`${pcHash}#${ethHash}`,`${socketHash}#${socketEthHash}`];
+    res.cables[cableHash] = { type: "twistedpair" };
+    res.connections[cableHash] = [`${pcHash}#${ethHash}`, `${socketHash}#${socketEthHash}`];
 
     res.devices[pcHash] = pc;
     res.devices[socketHash] = socket;
